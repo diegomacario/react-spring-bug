9f63bd79899e0efbebf6af192fa689984cf6cbd4
Diego Macario Bello <diego.macariobello@shopify.com>
2025-06-23 12:41:56 -0400

    Add drei.

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,15 @@ import ReactDOM from "react-dom/client";
 import "./style.css";
 import { Canvas } from "@react-three/fiber";
 import * as THREE from "three";
+import { OrbitControls } from "@react-three/drei";
 
 const root = ReactDOM.createRoot(
   document.querySelector("#root") as HTMLElement
 );
 
 const camera = new THREE.PerspectiveCamera(45, 1, 0.1, 50);
-camera.position.set(...new THREE.Vector3(0, 5, -5).toArray());
-camera.lookAt(0.0, 0, 0);
+camera.position.set(...new THREE.Vector3(5, 5, 5).toArray());
+camera.lookAt(0, 0, 0);
 
 root.render(
   <>
@@ -17,9 +18,10 @@ root.render(
       dpr={[1, 2]}
       camera={camera}
       gl={{ powerPreference: "high-performance", antialias: false }}
-      shadows={true}
+      shadows={false}
     >
       <color attach="background" args={["black"]} />
+      <OrbitControls />
       <axesHelper args={[1]} />
     </Canvas>
   </>
